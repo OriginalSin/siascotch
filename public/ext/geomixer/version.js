f7930e9f87216b12496c672c7df58c0d732be304
Serg <serg@auchat.ru>
2018-11-25 15:07:00 +0300

    upd

---
+++ public/ext/geomixer/version.js
@@ -0,0 +1,8 @@
+var gmxVersion = {
+  "jsPath": {
+    "//www.kosmosnimki.ru/lib/geomixer/geomixer-src.js": 1540637737708
+  },
+  "cssPath": {
+    "//www.kosmosnimki.ru/lib/geomixer/geomixer.css": 1540637737708
+  }
+};
